90927dce84055d40f253ad37c6084e678c33ba90
bayunimous <jigs4wcode@gmail.com>
2023-06-09 11:21:48 +0800

    [FIX]: Perbaikan pada config.php

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
 
-$conn = mysqli_connect('localhost', 'root', 'Bayu003x#', 'id20884594_shop_db ') or die('connection failed');
+$conn = mysqli_connect('localhost', 'root', '', 'shop_db') or die('connection failed');
